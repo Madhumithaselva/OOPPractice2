5d0dd8aee16f01a3c378f6e0278124c5cd9231da
Madhu <madhu8129@gmail.com>
2023-12-12 11:50:12 +0100

    Ex1 created:Employee clss created

---
+++ src/main/java/se/lexicon/Main.java
@@ -0,0 +1,11 @@
+package se.lexicon;
+import se.lexicon.model.Employee;
+
+import java.lang.Object;
+import java.lang.String;
+
+public class Main {
+    public static void main(String[] args) {
+        Employee emp1=new Employee();
+    }
+}
